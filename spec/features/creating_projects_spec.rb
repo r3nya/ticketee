ec941a960fdc5e85668b662a54c2e1560432046d
Andrew M <me@r3nya.ru>
2013-12-04 00:44:42 +0400

    3.4.5 Add validation to ensure names are specified when creating projects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
-  scenario 'can create a project' do
+  before do
     visit '/'
-
     click_link 'New Project'
+  end
 
+  scenario 'can create a project' do
     fill_in 'project_name', with: 'MacVim'
     fill_in 'Description', with: 'A awesome text-editor for OS X'
 
@@ -19,6 +20,12 @@ feature 'Creating Projects' do
 
     title = "MacVim - Projects - Ticketee"
     expect(page).to have_title(title)
+  end
+
+  scenario 'can not create a project without a name' do
+    click_button 'Create Project'
+    expect(page).to have_content('Project has not been created.')
+    expect(page).to have_content("Name can't be blank")
   end
 end
 
