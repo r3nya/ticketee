f68275aac3ab9f1e6df6c5aff285272ea7dc5251
Andrew M <me@r3nya.ru>
2014-02-06 01:15:43 +0400

    5.1 - Implement creating tickets for a project

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
 	before_action :set_project
+	before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
 	def new
 		@ticket = @project.tickets.build
@@ -20,6 +21,10 @@ class TicketsController < ApplicationController
 
 		def set_project
 			@project = Project.find(params[:project_id])
+		end
+
+		def set_ticket
+			@ticket = @project.tickets.find(params[:id])
 		end
 
 		def ticket_params
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,5 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
+  validates	:title, :description, presence: true
+  validates :description, length: { minimum: 10 }
 end
